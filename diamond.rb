a04e3cd2e4ecae49f21c66cbf53790425c005dcf
Tom Koch <tomck@users.noreply.github.com>
2016-02-27 18:39:11 -0600

    Update diamond.rb

--- diamond.rb
+++ diamond.rb
@@ -5,11 +5,10 @@ class Diamond < Formula
   # tag "bioinformatics"
 
   url "https://github.com/bbuchfink/diamond/archive/v0.7.11.tar.gz"
-  version "0.7.11"
   sha256 "e486bed71d5ea1f73c6016504cf137b74ff710422bb7f0038e16360bfb00b2f5"
 
   bottle do
-    sha256 "e486bed71d5ea1f73c6016504cf137b74ff710422bb7f0038e16360bfb00b2f5" => :yosemite
+    sha256 "e0eb3edc6f875a6b8e37b2d369b27510ec714faf2e94ff414edb94fbd34a1141" => :yosemite
     sha256 "da0750e96465902fbd7827dc2220c2cb298f71ae488d6175885eb2044e2066ad" => :mavericks
     sha256 "459c5a98274de600b7a270e51a589c21bae1add6384fe76d4a4b5c3d988778cb" => :mountain_lion
   end
